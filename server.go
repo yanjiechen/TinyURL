7357d8e932c8376f1344168060ba40dd08ad9930
KitChen <13581702675@qq.com>
2016-10-11 17:15:22 +0800

    Short URL Demo source code

--- server.go
+++ server.go
@@ -9,7 +9,7 @@ import (
     "net/http"
 )
 
-var list map[string]string
+var list = map[string]string{}
 
 var charTable = [...]rune{
     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
@@ -20,55 +20,51 @@ var charTable = [...]rune{
     'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
 }
 
-func index(w http.ResponseWriter, r *http.Request) {
-    io.WriteString(w, "It works!")
-}
-
-func shorten(w http.ResponseWriter, r *http.Request) {
-    r.ParseForm()
-    url, ok := r.Form["url"]
-    result := map[string]string{}
-
-    if ok && url[0] != "" {
-        sumData := md5.Sum([]byte(url[0]))
-        partUint := binary.BigEndian.Uint32(sumData[0 : 4])
-        shortBuffer := &bytes.Buffer{}
-        for i := 0; i < 6; i++ {
-            shortBuffer.WriteRune(charTable[partUint % 62])
-            partUint = partUint >> 5
-        }
-        short := "http://" + r.Host + "/" + shortBuffer.String()
-
-        list[short] = url[0]
-        result["short"] = short
-    } else {
-        result["err"] = "param url empty"
-    }
-
-    bytes, _ := json.Marshal(result)
-    io.WriteString(w, string(bytes))
-}
-
-func original(w http.ResponseWriter, r *http.Request) {
-    r.ParseForm()
-    short, ok := r.Form["short"]
-    result := map[string]string{}
-
-    if ok && short[0] != "" {
-        original := list[short[0]]
-        result["original"] = original
-    } else {
-        result["err"] = "param short empty"
-    }
-
-    bytes, _ := json.Marshal(result)
-    io.WriteString(w, string(bytes))
-}
-
 func main() {
-    list = map[string]string{}
-    http.HandleFunc("/", index)
-    http.HandleFunc("/shorten", shorten)
-    http.HandleFunc("/original", original)
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        io.WriteString(w, "It works!")
+    })
+
+    http.HandleFunc("/shorten", func(w http.ResponseWriter, r *http.Request) {
+        r.ParseForm()
+        url, ok := r.Form["url"]
+        result := map[string]string{}
+
+        if ok && url[0] != "" {
+            sumData := md5.Sum([]byte(url[0]))
+            partUint := binary.BigEndian.Uint32(sumData[0 : 4])
+            shortBuffer := &bytes.Buffer{}
+            for i := 0; i < 6; i++ {
+                shortBuffer.WriteRune(charTable[partUint % 62])
+                partUint = partUint >> 5
+            }
+            short := "http://" + r.Host + "/" + shortBuffer.String()
+
+            list[short] = url[0]
+            result["short"] = short
+        } else {
+            result["err"] = "param url empty"
+        }
+
+        bytes, _ := json.Marshal(result)
+        io.WriteString(w, string(bytes))
+    })
+
+    http.HandleFunc("/original", func(w http.ResponseWriter, r *http.Request) {
+        r.ParseForm()
+        short, ok := r.Form["short"]
+        result := map[string]string{}
+
+        if ok && short[0] != "" {
+            original := list[short[0]]
+            result["original"] = original
+        } else {
+            result["err"] = "param short empty"
+        }
+
+        bytes, _ := json.Marshal(result)
+        io.WriteString(w, string(bytes))
+    })
+
     http.ListenAndServe(":80", nil)
 }
